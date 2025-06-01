01f0621756d3d913d6022df9f37bbc565b1c5b66
Le Quang Thang <lethang7794@gmail.com>
2025-06-01 18:16:44 +0700

    Fix crawl script fail to get description for some services

--- scripts/aws-services-docs-crawler.ts
+++ scripts/aws-services-docs-crawler.ts
@@ -153,8 +153,6 @@ function cleanPath(urlPath?: string) {
 
 // Function to extract detailed description from service page
 async function extractDetailInfo(context: BrowserContext, service: Service) {
-  const DECISION_GUIDES_CATEGORY = 'Decision Guides'
-
   const url = service.url
   const serviceName = service.service
 
@@ -168,83 +166,71 @@ async function extractDetailInfo(context: BrowserContext, service: Service) {
   let detailDescription = ''
   const sections = []
 
-  // For Decision Guides, the description is in a different place
-  if (service.categories.includes(DECISION_GUIDES_CATEGORY)) {
-    try {
-      detailDescription = await detailPage.$eval(
-        'table tbody tr td:nth-child(2) p',
-        (el: HTMLElement) => el.innerText.trim()
-      )
-    } catch (e) {
-      if (e instanceof Error) {
-        console.warn(
-          `No description found for ${JSON.stringify(service)} ${serviceName}: ${e.message}`
+  try {
+    // The detail description can be in various places, so we try multiple selectors
+    // - In the div after the title, e.g. https://docs.aws.amazon.com/apigateway/
+    // - or in the second cell of the first row of the table, e.g. https://docs.aws.amazon.com/decision-guides/latest/modern-apps-strategy-on-aws-how-to-choose/modern-apps-strategy-on-aws-how-to-choose.html
+    // - or in the first paragraph of the main content, e.g. https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html
+    const detailDescriptionEl = await detailPage.$(
+      'h1 ~ div, table tbody tr td:nth-child(2) p, div#main-content div#main-col-body p:first-of-type'
+    )
+    detailDescription = (await detailDescriptionEl?.innerText()) || ''
+
+    // Find all sections
+    const sectionsEl = await detailPage.$$('section')
+
+    // Iterate through each section
+    for (const sectionEl of sectionsEl) {
+      let section: Section
+
+      // Get section name
+      const sectionNameEl = await sectionEl.$('h2')
+      const sectionName = await sectionNameEl?.textContent()
+
+      // Find all items in the section
+      const itemsEl = await sectionEl.$$('li[data-selection-item]')
+      const items = []
+
+      // Iterate through each item
+      for (const itemEl of itemsEl) {
+        // Get item link
+        const itemLinkEl = await itemEl.$('h3 a')
+        const itemLink = await itemLinkEl?.getAttribute('href')
+
+        // Get item name
+        const itemNameEl = await itemEl.$('h3 a span, h3')
+        const itemName = await itemNameEl?.textContent()
+
+        // Get item description
+        const itemDescription = await itemEl.$eval(
+          'div div:nth-child(2) div div div',
+          (el: HTMLElement) => el.innerText.trim()
         )
+
+        // Collect item data
+        const item = {
+          name: itemName,
+          link: itemLink,
+          description: itemDescription,
+        }
+        // Push item data to items array
+        items.push(item)
       }
+
+      // Collect section data
+      section = {
+        name: sectionName!,
+        items: items,
+      }
+
+      // Push section data to sections array
+      sections.push(section)
     }
-  } else {
-    try {
-      detailDescription = await detailPage.$eval(
-        'h1 ~ div',
-        (el: HTMLElement) => el.innerText.trim()
+  } catch (e) {
+    if (e instanceof Error) {
+      console.warn(
+        `No description found for ${JSON.stringify(service)} ${serviceName}: ${e.message}`
       )
-
-      // Find all sections
-      const sectionsEl = await detailPage.$$('section')
-
-      // Iterate through each section
-      for (const sectionEl of sectionsEl) {
-        let section: Section
-
-        // Get section name
-        const sectionNameEl = await sectionEl.$('h2')
-        const sectionName = await sectionNameEl?.textContent()
-
-        // Find all items in the section
-        const itemsEl = await sectionEl.$$('li[data-selection-item]')
-        const items = []
-
-        // Iterate through each item
-        for (const itemEl of itemsEl) {
-          // Get item link
-          const itemLinkEl = await itemEl.$('h3 a')
-          const itemLink = await itemLinkEl?.getAttribute('href')
-
-          // Get item name
-          const itemNameEl = await itemEl.$('h3 a span, h3')
-          const itemName = await itemNameEl?.textContent()
-
-          // Get item description
-          const itemDescription = await itemEl.$eval(
-            'div div:nth-child(2) div div div',
-            (el: HTMLElement) => el.innerText.trim()
-          )
-
-          // Collect item data
-          const item = {
-            name: itemName,
-            link: itemLink,
-            description: itemDescription,
-          }
-          // Push item data to items array
-          items.push(item)
-        }
-
-        // Collect section data
-        section = {
-          name: sectionName!,
-          items: items,
-        }
-
-        // Push section data to sections array
-        sections.push(section)
-      }
-    } catch (e) {
-      if (e instanceof Error) {
-        console.warn(
-          `No description found for ${JSON.stringify(service)} ${serviceName}: ${e.message}`
-        )
-      }
     }
   }
 
